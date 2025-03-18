6c74ab02ff016d69c61bbe9d6a555ccd7be87385
Mathis Dardé <dardemathis@gmail.com>
2025-03-18 13:44:48 +0100

    fix(crucial): ajout de deux nouveaux schemas de type pour la nouvelle formule d'inscription

--- types/forms.ts
+++ types/forms.ts
@@ -1,10 +1,16 @@
-import { RegisterSchema } from "@/app/schema";
+import { RegisterParticulierSchema } from "@/app/schema";
+import { RegisterCommercantSchema } from "@/app/schema";
 import { LoginSchema } from "@/app/schema";
 import { DonSchema } from "@/app/schema";
 import { ValidateSchema } from "@/app/schema";
 import { z, ZodIssue } from "zod";
 
-export type RegisterSchemaType = z.infer<typeof RegisterSchema>;
+export type RegisterParticulierSchemaType = z.infer<
+  typeof RegisterParticulierSchema
+>;
+export type RegisterCommercantSchemaType = z.infer<
+  typeof RegisterCommercantSchema
+>;
 export type LoginSchemaType = z.infer<typeof LoginSchema>;
 export type DonSchemaType = z.infer<typeof DonSchema>;
 export type ValidateSchemaType = z.infer<typeof ValidateSchema>;
