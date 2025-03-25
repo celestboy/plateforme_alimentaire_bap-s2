f89587076432b4f0128fcd1c6bc013dced54c551
Célestin Godefroy <celestin.godefroy@gmail.com>
2025-03-25 14:18:54 +0100

    début intégration page single don

--- app/(navfooter)/dons/[id]/_components/DonDisplay.tsx
+++ app/(navfooter)/dons/[id]/_components/DonDisplay.tsx
@@ -20,21 +20,58 @@ interface DonProps {
 
 export default function DonDisplay({ don }: DonProps) {
   return (
-    <div className="bg-gray-100 min-h-screen w-full p-0 m-0 box-border">
-      <div className="flex justify-center gap-10">
-        <div className="w-[975px]">
-          <h2 className="font-Montserrat font-extrabold text-3xl">
-            {don.title}
-          </h2>
-          <Image
-            src={`${don.img_url}`}
-            width={1024}
-            height={1024}
-            alt="Image de bannière de l'article"
-            className="aspect-video w-full object-cover object-top mb-10 rounded-xl"
-          />
-        </div>
+    <div className="flex h-[70vh]">
+      <Image
+        src={`${don.img_url}`}
+        width={600}
+        height={1024}
+        alt="Image de bannière de l'article"
+        className="aspect-video w-1/3 h-max object-cover object-top m-24 rounded-xl"
+      />
+      <div className="p-16 h-4/5 w-1/3 border-[3px] border-[#717171] rounded-xl absolute right-36">
+        <h2 className="font-Montserrat font-extrabold text-3xl">{don.title}</h2>
+
+        <p className="my-4">
+          Date d'expiration : {don.limit_date.toLocaleDateString()}
+        </p>
+        <p className="my-4">Quantité : {don.quantity}</p>
+        <p className="my-4">
+          Lieux :{" "}
+          {don.rdv_pts
+            ? Array.isArray(don.rdv_pts)
+              ? don.rdv_pts
+                  .map((pt) =>
+                    typeof pt === "object" && pt !== null
+                      ? Object.values(pt).join(" ")
+                      : String(pt)
+                  )
+                  .join(", ")
+              : typeof don.rdv_pts === "object"
+              ? Object.values(don.rdv_pts).join(", ")
+              : String(don.rdv_pts)
+            : "Non spécifié"}
+        </p>
+
+        <p className="my-4">
+          Description : <br></br> {don.description}
+        </p>
       </div>
     </div>
+    // <div className="bg-gray-100 min-h-screen w-full p-0 m-0 box-border">
+    //   <div className="flex justify-center gap-10">
+    //     <div className="w-[975px]">
+    //       <h2 className="font-Montserrat font-extrabold text-3xl">
+    //         {don.title}
+    //       </h2>
+    //       <Image
+    //         src={`${don.img_url}`}
+    //         width={1024}
+    //         height={1024}
+    //         alt="Image de bannière de l'article"
+    //         className="aspect-video w-full object-cover object-top mb-10 rounded-xl"
+    //       />
+    //     </div>
+    //   </div>
+    // </div>
   );
 }
